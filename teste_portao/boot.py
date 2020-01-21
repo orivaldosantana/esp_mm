6ac59913c61d630cd2f21cdbf46fd023a282b11c
Orivaldo Santana Jr <orivaldo@gmail.com>
2020-01-21 12:17:18 -0300

    Add test files

---
+++ teste_portao/boot.py
@@ -0,0 +1,39 @@
+# Complete project details at https://RandomNerdTutorials.com
+
+import time
+from umqttsimple import MQTTClient
+import ubinascii
+import machine
+import micropython
+import network
+import esp
+esp.osdebug(None)
+import gc
+gc.collect()
+
+from machine import Pin
+#GPIO is equal to D2 
+gpio_rele1 = Pin(2, Pin.OUT)
+gpio_rele1.value(1); 
+
+client_id = ubinascii.hexlify(machine.unique_id())
+topic_sub = b'sitio/portao/rele1'
+topic_pub = b'sitio/portao/vivo'
+
+last_message = 0
+message_interval = 60
+counter = 0
+
+station = network.WLAN(network.STA_IF)
+
+station.active(True)
+station.connect(ssid, password)
+
+while station.isconnected() == False:
+  pass
+
+print('Connection successful')
+print(station.ifconfig())
+
+
+
